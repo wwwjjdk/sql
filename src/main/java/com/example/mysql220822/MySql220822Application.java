f674864d10697b1aa2e7bd3318c7a81aeba02096
wwwjjdk <goshasavtch@yandex.ru>
2022-08-24 23:45:25 +0300

    hm1sql

---
+++ src/main/java/com/example/mysql220822/MySql220822Application.java
@@ -0,0 +1,13 @@
+package com.example.mysql220822;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySql220822Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySql220822Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mysql220822/MySql220822ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysql220822;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySql220822ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
